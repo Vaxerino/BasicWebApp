7f35356c60e7fe6819f31bd93cd34450db2589c1
Simon Jokel <simon.jokel@tum.de>
2022-07-13 15:24:05 +0200

    largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,9 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 @Service
 public class QueryProcessor {
 
@@ -11,8 +14,15 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "pipe-it-up.herokuapp.com";
+        } else if (query.contains("numbers")) {
+            String[] numbers = query.split(":")[1].split(",");
+            int largest = Integer.MIN_VALUE;
+            for (String num : numbers){
+                if (Integer.parseInt(num)> largest){
+                    largest = Integer.parseInt(num);
+                }
+            }
+           return String.valueOf(largest);
         } else { // TODO extend the programm here
             return "";
         }
